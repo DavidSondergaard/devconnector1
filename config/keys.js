78a4d218116a8b4cc66705cad03de178bb59f4e8
TimeValue <dsondergaard@gmail.com>
2019-01-29 06:24:05 -0800

    initial express server with route files.
    we have setup the server with 3 route files,
    a mongoDB conection, some keys, etc.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://david:test1234@ds111050.mlab.com:11050/devconnector1"
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("hello!"));
+
+// DB Config
+const db = require("./config/keys.js").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db) //this is telling mongoose to connect to db
+  .then(() => console.log("MongoDB Connected")) //.then is a promise with success if it works
+  .catch(err => console.log(err)); // if it fails, this is how js promises work.
+
+//use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.get("/", (req, res) => res.send("Hello World!!!"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
