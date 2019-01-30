6c343490cd560cfcba98b1ff490b453888bd5df3
TimeValue <dsondergaard@gmail.com>
2019-01-29 16:30:03 -0800

    started working on gravitar

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,10 +14,26 @@ router.get("/test", (req, res) => res.json({ msg: "api users works" }));
 // @access  public
 router.post("/register", (req, res) => {
   //use mongoose to first find if the email exists.
-  /** use find one to find a single email record.  when we send data through a
-   * route  through a post request which will be through postman or a form, we
-   * access that data through req.body. */
-  User.findOne({ email: req.body.email });
+  /**use find one to find a single email record.  when we send data through a
+   * route  through a post request which will be through postman or a form,
+   * we access that data through req.body.
+   */
+  User.findOne({ email: req.body.email }) // can access this with bodyParser.json
+    .then(user => {
+      // we match the email passed in with find 1 with the promise (.then)
+      if (user) {
+        //if the user is found with the email address
+        return res.status(400).json({ email: "email already exists" }); //sending error
+      } else {
+        //make a new user
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+      } //for validation
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require("express"); //main framework
-const mongoose = require("mongoose"); //used to connect and interact with MongoDB
-const bodyParser = require("body-parser"); //take in data with requests and do what we want with them.
+const express = require("express"); // EDU: main framework
+const mongoose = require("mongoose"); // EDU: used to connect and interact with MongoDB
+const bodyParser = require("body-parser"); // EDU: take in data with requests and do what we want with them.
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -8,22 +8,27 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// EDU: Body parser middle-ware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // now we should be able  to access req.body.whatever
+
 app.get("/", (req, res) => res.send("hello!"));
 
-// DB Config
+// EDU: DB Config
 const db = require("./config/keys.js").mongoURI;
 
-//connect to MongoDB
+// EDU: connect to MongoDB
 mongoose
   .connect(db) //this is telling mongoose to connect to db
   .then(() => console.log("MongoDB Connected")) //.then is a promise with success if it works
   .catch(err => console.log(err)); // if it fails, this is how js promises work.
 
-//use Routes
+// EDU: use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
+// ? QUERY: Why do I have this here?
 app.get("/", (req, res) => res.send("Hello World!!!"));
 
 const port = process.env.PORT || 5000;
